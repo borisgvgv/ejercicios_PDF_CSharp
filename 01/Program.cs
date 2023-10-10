a660af97e98045708987b9357bd3527be8629de1
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2023-10-10 17:33:44 +0000

    act

---
+++ 01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Ejercicio_1 {
+    static void Main (){
+        int suma = 2 + 3;
+        Console.WriteLine("Holaa {0}", suma);
+    }
+}
