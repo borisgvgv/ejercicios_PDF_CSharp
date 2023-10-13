c2beeedb2eb3cee8212adba739514d969fc0d897
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2023-10-13 18:16:56 +0000

    act

---
+++ 2212/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+(2.2.1.1.2) Crea un "calculador de cuadrados": pedirá al usuario un número y
+mostrará su cuadrado. Se repetirá mientras el número introducido no sea cero
+(usa "while" para conseguirlo).
+*/
+
+using System;
+class E2212
+{
+    static void Main()
+    {
+        Console.WriteLine("Añade un número");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+
+        while (numero != 0)
+        {
+
+            Console.WriteLine(numero * numero);
+            Console.WriteLine("Añade un número");
+            numero = Convert.ToInt32(Console.ReadLine());
+            
+        }
+
+    }
+}
