86d9c9fd585c324ab573e2c8cc5ae8021e9108c2
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2023-12-06 19:52:57 +0000

    act

--- 527.1_paso _de _parametros_por_ref/Program.cs
+++ 527.1_paso _de _parametros_por_ref/Program.cs
@@ -10,11 +10,34 @@ class Ejemplo_05_07_02a
         x = x * 2;
         Console.WriteLine(" y ahora vale {0}", x);
     }
+
+    static void NumeroMayor(ref int a, ref int b)
+    {
+        int mayor = a;
+        if (b > a)
+        {
+            mayor = b;
+        }
+        Console.WriteLine("El número mayor es {0}", mayor);
+        
+    }
+
+    static void Intercambiar(ref int x, ref int y){
+        int a = y;
+        int b = x;
+        Console.WriteLine("Ahora a es  {0}", a);
+        Console.WriteLine("Ahora b es  {0}", b);
+    }
+
     static void Main()
     {
         int n = 5;
         Console.WriteLine("n vale {0}", n);
         Duplicar(ref n);
         Console.WriteLine("Ahora n vale {0}", n);
+
+        int num1 = 5, num2 = 7;
+        NumeroMayor(ref num1, ref num2);
+        Intercambiar(ref num1, ref num2);
     }
 }
