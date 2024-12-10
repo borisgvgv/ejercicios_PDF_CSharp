a70c4be89fee7499313e76852ad6d322e624ec96
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2024-12-10 10:52:55 +0000

    act

---
+++ 520.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+
+    public static void Saludar()
+    {
+        System.Console.WriteLine("Buenos días");
+        System.Console.WriteLine("Espero que estés bien");
+    }
+    static void Main()
+    {
+
+        Saludar();
+
+    }
+}
---
+++ 520.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+
+    public static void LimpiarConsola()
+    {
+        for (int i = 0; i <= 25; i++)
+        {
+            System.Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        LimpiarConsola();
+    }
+}
