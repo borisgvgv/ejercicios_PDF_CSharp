8964c0d4680f4f2af94be7ee7fe4ac1ec5aeccad
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2025-03-20 13:11:51 +0000

    ACT

---
+++ 7.3.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Persona[] persona = new Persona[3];
+        persona[0] = new Persona();
+        persona[1] = new PersonaInglesa();
+        persona[2] = new PersonaEspanola();
+
+        persona[0].Saludo();
+        persona[1].Saludo();
+        persona[2].Saludo();
+    }
+}
+
+public class Persona
+{
+    public virtual void Saludo()
+    {
+        System.Console.WriteLine("Hola soy una persona del mundo");
+    }
+}
+
+public class PersonaInglesa : Persona
+{
+    public override void Saludo()
+    {
+        System.Console.WriteLine("Hola soy una persona inglesa");
+    }
+}
+public class PersonaEspanola : Persona
+{
+    public override void Saludo()
+    {
+        System.Console.WriteLine("Hola soy una persona española");
+    }
+}
---
+++ 7.3_Virtual_Override/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Animal[] animal = new Animal[3];
+        animal[0] = new Animal();
+        animal[1] = new Perro();
+        animal[2] = new Gato();
+
+        animal[0].MostrarSaludo();
+        animal[1].MostrarSaludo();
+        animal[2].MostrarSaludo();
+    }
+}
+
+
+public class Animal
+{
+    public virtual void MostrarSaludo()
+    {
+        System.Console.WriteLine("Hola soy un animal");
+    }
+}
+
+public class Gato : Animal
+{
+    public override void MostrarSaludo()
+    {
+        System.Console.WriteLine("Hola soy un gato");
+    }
+}
+
+public class Perro : Animal
+{
+    public override void MostrarSaludo()
+    {
+        System.Console.WriteLine("Hola soy un perro");
+    }
+}
