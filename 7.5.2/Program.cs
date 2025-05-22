2129af5f80533204f52f40804af008c356cae2c2
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2025-05-22 11:01:37 +0000

    act

--- 7.5.2/Program.cs
+++ 7.5.2/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+(7.5.2) Crea una clase Puerta con un ancho, un alto y un método "MostrarEstado"
+que muestre su ancho y su alto. Crea una clase Casa, que contenga 3 puertas y
+otro método "MostrarEstado" que escriba "Casa" y luego muestre el estado de sus
+tres puertas.
+*/
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Casa casa = new Casa();
+        casa.MostrarEstado();
+    }
+}
+
+public class Puerta
+{
+    protected byte ancho;
+    protected byte alto;
+
+    public Puerta() : this(5, 2)
+    {
+    }
+    public Puerta(byte ancho, byte alto)
+    {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public void MostrarEstado()
+    {
+        System.Console.WriteLine("Ancho: {0}, Alto: {1}", ancho, alto);
+    }
+}
+
+public class Casa
+{
+
+    Puerta[] puertas = new Puerta[3];
+    public Casa()
+    {
+        for (int i = 0; i < puertas.Length; i++)
+        {
+            if (i == 0)
+            {
+                puertas[i] = new Puerta();
+            }
+            else
+            {
+                Console.WriteLine("Casa {0}", i + 1);
+                Console.Write("Ancho: ");
+                byte ancho = Convert.ToByte(Console.ReadLine());
+                Console.Write("Alto: ");
+                byte alto = Convert.ToByte(Console.ReadLine());
+                puertas[i] = new Puerta(ancho, alto);
+
+            }
+        }
+    }
+
+    public void MostrarEstado()
+    {
+        Console.WriteLine("Casa");
+        for (int i = 0; i < puertas.Length; i++)
+        {
+            Console.Write("Puerta {0}: ", i + 1);
+            puertas[i].MostrarEstado();
+        }
+    }
+}
+
---
+++ 7.5.3/Program.cs
@@ -0,0 +1,108 @@
+﻿/*
+(7.5.3) Crea una clase Casa, con una superficie (por ejemplo, 90 m2) y un método
+"MostrarEstado" que escriba su superficie. Cada casa debe contener 3 puertas. Las
+puertas tendrán un ancho, un alto y un método "MostrarEstado" que muestre su
+ancho y su alto y la superficie de la casa en la que se encuentran. Crea un
+programa de prueba que cree una casa y muestre sus datos y los de sus tres
+puertas.
+*/
+// Me ha costado tres putos dias...
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Puertas casa1 = new Puertas();
+        casa1.MostrarEstado();
+
+    }
+}
+
+// ---------------------------------------------
+
+public class Casa
+{
+    protected int superficie;
+    public Casa() : this(90)
+    {
+
+    }
+    public Casa(int superficie)
+    {
+        this.superficie = superficie;
+    }
+
+    public void MostrarEstado()
+    {
+        System.Console.WriteLine($"{superficie} m\u00B2");
+    }
+
+}
+// ---------------------------------------------
+public class Puertas
+{
+    protected int cantidad;
+    protected int[,] ancho = new int[3, 3];
+    protected int[,] alto = new int[3, 3];
+    protected Casa[] casas = new Casa[9];
+    public Puertas() : this(500, 200)
+    {
+    }
+    public Puertas(int ancho, int alto)
+    {
+        System.Console.WriteLine("Cuantas casa quiere añadir");
+        cantidad = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("La superficie de la casa 1 y las dimenciones de la puerta 1 de cada casa son valores fijos predeterminados");
+        System.Console.WriteLine();
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            if (i == 0)
+            {
+                casas[i] = new Casa();
+            }
+            else
+            {
+                System.Console.Write($"Indica Superficie casa {i + 1}: ");
+                int superficie = Convert.ToInt32(Console.ReadLine());
+                casas[i] = new Casa(superficie);
+            }
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (j == 0)
+                {
+                    this.ancho[i, j] = ancho;
+                    this.alto[i, j] = alto;
+                }
+                else
+                {
+                    System.Console.Write($"Puerta {j + 1} casa {i + 1} Ancho : ");
+                    this.ancho[i, j] = Convert.ToInt32(Console.ReadLine());
+                    System.Console.Write($"Puerta {j + 1} casa {i + 1} Alto: ");
+                    this.alto[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+
+    public void MostrarEstado()
+    {
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            System.Console.Write($" Superficie Casa {i + 1}: ");
+            casas[i].MostrarEstado();
+            for (int j = 0; j < 3; j++)
+            {
+                System.Console.WriteLine($"Ancho: {ancho[i, j]} cm, Alto: {alto[i, j]} cm");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
