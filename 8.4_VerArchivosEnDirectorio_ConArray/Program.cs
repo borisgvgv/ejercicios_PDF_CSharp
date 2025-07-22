67219a4d3d8831825996e62df5dbd99d2c3257ed
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2025-07-22 11:11:40 +0000

    act

--- 8.4_VerArchivosEnDirectorio_ConArray/Program.cs
+++ 8.4_VerArchivosEnDirectorio_ConArray/Program.cs
@@ -7,7 +7,7 @@ public class Program
     public static async Task Main()
     {
         Console.Clear();
-        
+
         //Utilizar using System.IO para poder trabajar con directorios y archivos
         string directorio = @"/workspaces/ejercicios_PDF_CSharp/8.4_AñadirAUnFicheroExistente_ConClases";
         string extension = "*.txt";
@@ -31,6 +31,13 @@ public class Program
             foreach (string archivo in archivosTxtEncontrados)
             {
                 System.Console.WriteLine(Path.GetFileName(archivo));
+
+            /*
+            Otras variantes útiles:
+            Path.GetExtension(path) → Obtiene la extensión (por ejemplo, .txt)
+            Path.GetFileNameWithoutExtension(path) → Nombre sin la extensión (ejemplo en vez de ejemplo.txt)
+            Path.GetDirectoryName(path) → Obtiene solo la carpeta de la ruta
+            */
             }
 
         }
